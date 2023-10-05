3a9bf226d7325c7b936f5e72deb980213a099681
TULIMAT Anas OBS/GDO <TZKM2460@orange.com>
2023-10-05 18:37:08 +0200

    feat(front): set up api slice

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,11 @@
 import {configureStore} from "@reduxjs/toolkit";
 import authReducer from './slices/authSlice.js';
+import {apiSlice} from './slices/apiSlice.js';
 
 const store = configureStore({
     reducer: {
         auth: authReducer,
+        [apiSlice.reducerPath]: apiSlice.reducer
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
     devTools: true
